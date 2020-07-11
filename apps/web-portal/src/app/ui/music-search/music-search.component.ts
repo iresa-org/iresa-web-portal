c63990d97ccc22aa248062d6717019649ad012ca
Khoi Bui <emquacau123472>
2020-07-11 17:45:52 -0400

    Add Git Action workflow

--- apps/web-portal/src/app/ui/music-search/music-search.component.ts
+++ apps/web-portal/src/app/ui/music-search/music-search.component.ts
@@ -2,7 +2,7 @@ import {
   Component,
   OnInit,
   ChangeDetectionStrategy,
-  OnDestroy
+  OnDestroy,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { debounceTime, tap, startWith, filter } from 'rxjs/operators';
@@ -10,13 +10,12 @@ import { DashboardFacade } from '@iresa/web-portal-data';
 import { SubSink } from 'subsink';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Router } from '@angular/router';
-import { environment } from '@iresa/web-portal/env';
 
 @Component({
   selector: 'iresa-portal-music-search',
   templateUrl: './music-search.component.html',
   styleUrls: ['./music-search.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MusicSearchComponent implements OnInit, OnDestroy {
   searchInput = new FormControl();
@@ -42,14 +41,14 @@ export class MusicSearchComponent implements OnInit, OnDestroy {
         .pipe(
           debounceTime(500),
           startWith(''),
-          filter(val => val && typeof val === 'string' && val.trim() !== ''),
-          tap(value => this.dbFacade.search(value.trim()))
+          filter((val) => val && typeof val === 'string' && val.trim() !== ''),
+          tap((value) => this.dbFacade.search(value.trim()))
         )
         .subscribe()
     );
   }
 
-  displayFn = item => {
+  displayFn = (item) => {
     if (item) {
       return item.name;
     }
@@ -61,25 +60,25 @@ export class MusicSearchComponent implements OnInit, OnDestroy {
       artist: this.fetchArtistAlbums,
       album: this.fetchAlbum,
       track: this.fetchAlbumTracks,
-      playlist: this.fetchPlaylistTracks
+      playlist: this.fetchPlaylistTracks,
     };
     if (val.type in mapFn) {
       mapFn[val.type](val);
       this.dbFacade.setSelectedMenuItems('');
     }
   }
-  fetchAlbumTracks = track => {
+  fetchAlbumTracks = (track) => {
     this.router.navigate(['/album', track.album.id, track.track_number]);
   };
-  fetchAlbum = album => {
+  fetchAlbum = (album) => {
     this.router.navigate(['/album', album.id]);
   };
 
-  fetchArtistAlbums = artist => {
+  fetchArtistAlbums = (artist) => {
     this.router.navigate(['/artist', artist.id, 'albums']);
   };
 
-  fetchPlaylistTracks = playlist => {
+  fetchPlaylistTracks = (playlist) => {
     this.router.navigate(['/playlist', playlist.id, 'tracks']);
   };
 }
